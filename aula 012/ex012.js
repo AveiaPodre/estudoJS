5c399e8a41a9770c71c38a804aba2ca756c4b7d1
Averinaldo Júnior <av_jr@outlook.com.br>
2024-08-23 14:30:45 -0300

    :books: aula 12 finalizada

---
+++ aula 012/ex012.js
@@ -0,0 +1,12 @@
+var idade = 20
+console.log(`${idade} anos`)
+
+if(idade >= 18 && idade < 65){
+    console.log("Vota")
+}
+else if(idade >= 16){
+    console.log("Voto opcional")
+}
+else{
+    console.log("Não vota")
+}
