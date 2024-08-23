b7917f53bad1a7009c399456624bbaf6ecb20f0e
Averinaldo Júnior <av_jr@outlook.com.br>
2024-08-23 13:55:20 -0300

    :books:aulas até 11 terminadas

---
+++ aula 011/ex009.js
@@ -0,0 +1,10 @@
+var vel = 90
+var limite = 80
+
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if(vel > limite || vel < limite/2){
+    console.log(`MULTA`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aula 011/ex010.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+//país = 'Brasil'
+if(país == 'Brasil'){
+    console.log('Brasileiro')
+}
+else{
+    console.log('Estrangeiro')
+}
